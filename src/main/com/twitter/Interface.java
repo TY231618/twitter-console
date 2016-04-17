9e763d50b373acdecd83956f77f8ea3764eb2d4d
Tony Young <ty_training@hotmail.co.uk>
2016-04-17 21:23:04 +0100

    Added some switch cases to interface

--- src/main/com/twitter/Interface.java
+++ src/main/com/twitter/Interface.java
@@ -1,13 +1,18 @@
 package main.com.twitter;
 
 
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
+
 public class Interface {
 
     public static void main(String args[]) {
        optionCommands();
+       commands();
     }
 
-    private static void optionCommands() {
+    public static void optionCommands() {
         System.out.println("User Options");
         System.out.println("1. Create User");
         System.out.println("2. Post Tweet");
@@ -15,4 +20,43 @@ public class Interface {
         System.out.println("4. Follow User");
         System.out.println("5. Exit");
     }
+
+    private static void commands() {
+        switch (parseInt(scanner())) {
+            case 1:
+                createUser();
+                break;
+            case 2:
+                createPost();
+                break;
+            case 3:
+                exit();
+                break;
+        }
+    }
+
+    private static String scanner() {
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
+
+    private static void createUser() {
+        List<String> users = new ArrayList<String>();
+        System.out.println("Enter your Username: ");
+        users.add(scanner());
+        System.out.println("Pick another option from the menu:");
+        commands();
+    }
+
+    private static void createPost() {
+        List<String> post = new ArrayList<String>();
+        System.out.println("Enter Tweet: ");
+        post.add(scanner());
+        System.out.println("Pick another option from the menu:");
+        commands();
+    }
+
+    private static void exit() {
+    }
+
 }
--- src/test/com/twitter/InterfaceTest.java
+++ src/test/com/twitter/InterfaceTest.java
@@ -1,12 +1,15 @@
 package test.com.twitter;
 
 
-import main.com.twitter.Interface;
-
+import org.junit.Test;
 
 public class InterfaceTest {
 
-    Interface twitter = new Interface();
 
+
+    @Test
+    public void viewCommands() {
+
+    }
 
 }
