8c76d3dfc6138fed0a115490ebc4ea3e59c44e99
Tony Young <ty_training@hotmail.co.uk>
2016-04-17 19:47:18 +0100

    Interface with commands, no Switch added and untested

---
+++ src/main/com/twitter/Interface.java
@@ -0,0 +1,18 @@
+package main.com.twitter;
+
+
+public class Interface {
+
+    public static void main(String args[]) {
+       optionCommands();
+    }
+
+    private static void optionCommands() {
+        System.out.println("User Options");
+        System.out.println("1. Create User");
+        System.out.println("2. Post Tweet");
+        System.out.println("3. View Wall");
+        System.out.println("4. Follow User");
+        System.out.println("5. Exit");
+    }
+}
---
+++ src/test/com/twitter/InterfaceTest.java
@@ -0,0 +1,13 @@
+package test.com.twitter;
+
+
+import main.com.twitter.Interface;
+import org.junit.Test;
+
+public class InterfaceTest {
+
+    Interface twitter = new Interface();
+
+    @Test
+
+}
